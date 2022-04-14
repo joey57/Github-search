04d2ddee2af319b5bc71379315c1bffd4bd4b292
joey57 <njorogejoey5@gmail.com>
2022-04-14 12:36:22 +0300

    display my github repos

--- src/app/component/user/user.component.ts
+++ src/app/component/user/user.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchRequestService } from 'src/app/search-request.service';
+import { User } from 'src/app/user';
+import { Repository } from 'src/app/repository';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
+  providers: [SearchRequestService]
 })
 export class UserComponent implements OnInit {
+  public searchMe = 'joey57';
+  public githubUser = '';
 
-  constructor() { }
+  users!: User;
+  repository!: Repository;
+  public searchRepo!: string;
+  public resultCount =12;
 
-  ngOnInit(): void {
+  findUser(username:any){
+    this.githubUser ='';
+    this.searchMe = username;
+    this.ngOnInit();
+  }
+
+  constructor(public githubUserRequest: SearchRequestService, public userRepos: SearchRequestService) { }
+
+  ngOnInit() {
+    this.githubUserRequest.githubUser(this.searchMe);
+    this.users = this.githubUserRequest.users;
+    this.userRepos.gitUserRepos(this.searchMe);
+    console.log(this.userRepos);
+  }
+
+  searchRepos(){
+    this.searchRepo ='';
+    this.resultCount = 10;
   }
 
 }

61752bc84a89bc5502e5a90a58bb6f11a6bdd89b
joey57 <njorogejoey5@gmail.com>
2022-04-14 10:26:54 +0300

    added api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  myApi: "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"
 };
 
 /*

32e40b5b67945d8c82d2489e439071f6b9c95262
joey57 <njorogejoey5@gmail.com>
2022-04-15 10:11:40 +0300

    added about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './component/user/user.component';
 import { RepositoryComponent } from './component/repository/repository.component';
 import { NotFoundComponent } from './component/not-found/not-found.component';
+import { AboutComponent } from './component/about/about.component';
 
 const routes: Routes = [
   {path: 'users' , component:UserComponent},
   {path: 'repositories' , component:RepositoryComponent},
   {path: '', redirectTo:'/users', pathMatch:'full'},
   {path: '**', component:NotFoundComponent },
+  {path: 'about', component:AboutComponent}
 ];
 
 @NgModule({
